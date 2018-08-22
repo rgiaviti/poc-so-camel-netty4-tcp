b2ea8187cf384671514c9e28e33dd1b25d5e6cd1
Ricardo Giaviti <ricardo.giaviti@sensedia.com>
2018-08-22 09:53:45 -0300

    Solution

--- src/main/java/com/github/so/camel/routes/Netty4TcpRoute.java
+++ src/main/java/com/github/so/camel/routes/Netty4TcpRoute.java
@@ -1,26 +1,27 @@
 package com.github.so.camel.routes;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Netty4TcpRoute extends RouteBuilder {
 
-  private static final Logger log = LoggerFactory.getLogger(Netty4TcpRoute.class);
+  private static final String CAMEL_FROM_URL = "netty4:tcp://localhost:7000?textline=true&reuseChannel=true&encoding=utf8";
+  private static final String CAMEL_TO_URL = "bean:messageService";
+
+  private Processor messageProcessor;
+
+  @Autowired
+  public Netty4TcpRoute(@Qualifier("messageProcessor") Processor messageProcessor) {
+    this.messageProcessor = messageProcessor;
+  }
 
   @Override
   public void configure() throws Exception {
-    this.from("netty4:tcp://localhost:7000?textline=true&encoding=utf8")
-        .process(new Processor() {
-          @Override
-          public void process(final Exchange exchange) throws Exception {
-            log.info("[Processor] - Incoming Message -> {}", exchange.getIn().getBody(String.class));
-          }
-        }).to("bean:messageService");
+    this.from(CAMEL_FROM_URL).process(this.messageProcessor).to(CAMEL_TO_URL);
   }
 
 }
--- src/main/java/com/github/so/services/MessageService.java
+++ src/main/java/com/github/so/services/MessageService.java
@@ -2,8 +2,10 @@ package com.github.so.services;
 
 import java.util.LinkedList;
 import java.util.List;
+import io.netty.channel.Channel;
 import org.apache.camel.Exchange;
 import org.apache.camel.Handler;
+import org.apache.camel.component.netty4.NettyConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,13 +16,21 @@ public class MessageService {
   private static final Logger log = LoggerFactory.getLogger(MessageService.class);
 
   private List<String> messageStore = new LinkedList<>();
+  private Channel openedChannel;
   
   public void sendToTCP(final String message) {
     log.info("[Service] - Sending Message over TCP Channel --> {}", message);
+    log.info("[Service] - Channel is Active? {}", this.openedChannel.isActive());
+    log.info("[Service] - Channel is Open? {}", this.openedChannel.isOpen());
+    log.info("[Service] - Channel is Writeble? {}", this.openedChannel.isWritable());
+
+    this.openedChannel.write(message);
+    this.openedChannel.flush();
   }
 
   @Handler
   public void receiveFromTCP(final Exchange exchange) {
+    this.openedChannel = exchange.getProperty(NettyConstants.NETTY_CHANNEL, Channel.class);
     final String messageFromTcp = exchange.getIn().getBody(String.class);
     log.info("[Service] - Message Received from TCP Channel --> {}", messageFromTcp);
     this.messageStore.add(messageFromTcp);
